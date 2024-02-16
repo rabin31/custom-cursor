d38a274c7a0be14f5db7bb146c927ab4d6b7dc51
Rabin Thapa <121718603+rabin31@users.noreply.github.com>
2024-02-16 11:03:22 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let main = document.querySelector("#main");
+let cur = document.querySelector(".cursor");
+
+main.addEventListener("mousemove", function(dets){
+          cur.style.left = dets.x+"px"
+          cur.style.top = dets.y+"px"
+})
+
